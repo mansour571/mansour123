f63e59726a45201ab8a6c8b669ce56d5541d2705
mansour <mansour@33.com>
2022-11-26 20:33:50 +0200

    build_2

--- Mansour.java
+++ Mansour.java
@@ -1,8 +1,8 @@
 public class Mansour {
     public static void main(String[] args) {
 
-       System.out.print("mohamed naser jamal mansour");
-    
+        System.out.print("mohamed naser jamal mansour");
+
     }
-    
+
 }
